e868ec124afa93fefd90ae1399816f56a20c6973
Pedro N <itiropedro@gmail.com>
2026-08-24 12:45:57 -0300

    init

---
+++ lua/nvim-tree-sitter-tpp/init.lua
@@ -0,0 +1,31 @@
+--- @type table<string, any>
+local M = {}
+
+--- @param opts? {}
+function M.setup(opts)
+  vim.filetype.add({
+    extension = { tpp = "tpp" },
+  })
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "TSUpdate",
+    callback = function()
+      require("nvim-treesitter.parsers").tpp = {
+        install_info = {
+          url = "https://github.com/Itiro-P/tree-sitter-tpp",
+          revision = "0.1.1",
+          queries = "queries",
+          locals = "locals",
+        },
+      }
+    end,
+  })
+
+  require("nvim-treesitter").install({ "tpp" })
+
+  --vim.api.nvim_create_autocmd("FileType", {
+    
+  --})
+end
+
+return M
